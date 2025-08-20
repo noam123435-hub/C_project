2d6d87cf84f975cc9a8fca8b31299b8d340a1800
noam123435-hub <noam123435@gmail.com>
2025-08-21 02:03:11 +0300

    Add files via upload

---
+++ else1/else1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace else1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number;
+            number = 1;
+            while (number<=100)
+            {
+                if (number % 2 == 0)
+                {
+                    if (number % 6 == 0)
+                    {
+                        Console.WriteLine("boom");
+                    }
+                    else
+                    {
+                        Console.WriteLine(2);
+                    }
+                }
+                else
+                {
+                    if (number % 3 == 0)
+                    {
+                        Console.WriteLine(3);
+                    }
+                    else
+                    {
+                        Console.WriteLine(number);
+                    }
+                }
+                number = number + 1;
+            }
+        }
+    }
+}
---
+++ fff/fff/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace fff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please enter number and press ENTER");
+            int newnumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int number = newnumber;
+            int n = 0;
+
+            if (number > 0)
+            {
+                if (number > 100)
+                {
+                    n = (number % 10);
+                    number = number / 10;
+                    sum = sum + n;
+                }
+                if (number > 10)
+                {
+                    n= (number % 10);
+                    number = number / 10;
+                    sum = sum + n;
+                }
+                if (number < 10)
+                    sum = sum + number;
+            }
+            Console.WriteLine("The opposite number is " + sum);
+            if (newnumber==sum)
+                Console.WriteLine("the number "+newnumber+" is Shemesh");
+            
+
+
+        }
+    }
+}
