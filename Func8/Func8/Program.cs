5ab0aa0df824daf87619594cffe1bc1fbbfe8e07
noam123435-hub <noam123435@gmail.com>
2025-08-21 02:12:45 +0300

    Add files via upload

---
+++ Func8/Func8/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Func8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please enter the text to upper and press ENTER");
+            string Text=Console.ReadLine();
+            Console.WriteLine(toUpperText(Text));
+        }
+        static string toUpperText(string text)
+        {
+            string UpperText = "";
+            for(int i=0;i<text.Length;i++)
+            UpperText += toUpperChar(text[i]);
+            return UpperText;
+        }
+        static char toUpperChar(char tav)
+        {
+            string LowerCase = "abcdefghijklmnopqrstuvwxyz";
+            string Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            for (int i = 0; i < LowerCase.Length; i++)
+            {
+                if (LowerCase[i] == tav)
+                    return Uppercase[i];
+            }
+            return tav;
+        }
+    }
+}
---
+++ FuncBMI/FuncBMI/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FuncBMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, plese enter your weight in kg and press ENTER");
+            double Weight=double.Parse(Console.ReadLine());
+            Console.WriteLine("please enter your heightin meters and press ENTER");
+            double Hieght=double.Parse(Console.ReadLine());
+            Console.WriteLine("your BMI is " + bmi(Weight, Hieght));
+        }
+        static double bmi(double weight, double hieght)
+        {
+            return weight / (hieght * hieght);
+        }
+    }
+}
